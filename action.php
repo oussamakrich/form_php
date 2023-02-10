e4a5ef12be9258be3e3800f688259bd33bc9ff40
Oussama Krich <okrich@e1r5p9.1337.ma>
2023-02-10 12:57:09 +0100

    first

---
+++ action.php
@@ -0,0 +1,15 @@
+<?php
+
+
+echo "<h1> your info </h1>";
+// $tmp = $_POST['email'];	
+$tmp = $_REQUEST['email'];
+echo  "your email : $tmp","<br>";
+echo  "your password :" , $_POST["password"], "<br>";
+
+
+
+
+
+?>
+
