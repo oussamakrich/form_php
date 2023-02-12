22b96fd90f02d2ed119a3b7cc4b6cb330c5a4d44
oussama <sakawioussama@gmail.com>
2023-02-12 16:30:24 +0000

    fix all

--- action.php
+++ action.php
@@ -3,16 +3,28 @@
 
 echo "<h1> your info </h1>";
 
-$username = "oussama";
-$password = $username;
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$db_username = "oussama";
+$db_password = $db_username;
 $servername = "localhost";
 $database = "form";
 
-$con = new mysqli($servername, $username, $password, $database);
+$con = new mysqli($servername, $db_username, $db_password, $database);
+
 
 if (!$con->connect_error)
 {
-
+	$sql = "select * from form where email='$email' and password=md5('$password')";			
+	$res = $con->query($sql);
+	if ($res->num_rows > 0)
+	{
+		$row = $res->fetch_assoc();
+		echo "Welcome " . $row['fname'] . "  " . $row['lname'];
+	}
+	else
+		echo "invalid compt";	
 }
 
 else {
